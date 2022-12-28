c0b5d0d5ad3cf0ea03f6ce8c50153129d3de58c4
Marko Nikolaš <marko.nikolas@protonmail.com>
2022-12-28 13:08:15 +0100

    Update nav

--- components/Nav/Navbar.tsx
+++ components/Nav/Navbar.tsx
@@ -17,16 +17,16 @@ const Navbar = () => {
 				className={`${
 					menuState ? 'flex glass' : 'hidden'
 				} flex-col absolute bg-[#090909] z-50 top-[112px] left-0 w-screen h-[calc(100vh-112px)] justify-between py-[80px] sm:py-[120px] md:py-0 md:flex md:flex-row md:static md:w-auto md:h-auto md:bg-transparent gap-8 items-center text-sm tracking-widest font-jost`}>
-				<Link href='/' className='hover:text-gray-100 transition-colors text-2xl md:text-base'>
+				<Link href='/#welcome' className='hover:text-gray-100 transition-colors text-2xl md:text-base'>
 					Home
 				</Link>
-				<Link href='/work' className='hover:text-gray-100 transition-colors text-2xl md:text-base'>
+				<Link href='/#work' className='hover:text-gray-100 transition-colors text-2xl md:text-base'>
 					Work
 				</Link>
-				<Link href='/about' className='hover:text-gray-100 transition-colors text-2xl md:text-base'>
+				<Link href='/#about' className='hover:text-gray-100 transition-colors text-2xl md:text-base'>
 					About
 				</Link>
-				<Link href='/contact' className='hover:text-gray-100 transition-colors text-2xl md:text-base'>
+				<Link href='/#contact' className='hover:text-gray-100 transition-colors text-2xl md:text-base'>
 					Contact
 				</Link>
 			</nav>
