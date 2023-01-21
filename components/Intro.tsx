1b35976580b098d77563e1c75f2accc8f5e55d6c
Peter Pumpkineater <n1-dev+pumpkineater6996@simplelogin.co>
2023-01-21 16:27:27 -0500

    Update intro component typography and layout

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -4,6 +4,8 @@ import { useRef, useEffect } from "react";
 
 import { AnimationElementProps } from "./types";
 
+import { gsap } from "gsap";
+
 const Intro = (props: AnimationElementProps) => {
   const el = useRef(null);
 
@@ -18,11 +20,9 @@ const Intro = (props: AnimationElementProps) => {
   return (
     <section
       ref={el}
-      className="relative z-10 font-barlow text-white max-w-7xl mx-auto w-full p-8 md:p-20 translate-y-[10px] opacity-0 h-screen"
+      className="group z-10 font-barlow text-white max-w-7xl mx-auto w-full p-8 md:p-20 translate-y-[10px] opacity-0 h-screen"
     >
-      <h1 className="font-black text-[10vmin] italic tracking-widest">
-        Miloš Lakićević
-      </h1>
+      <h1 className="name font-black text-[10vmin]">Miloš Lakićević</h1>
       <h2 className="uppercase tracking-[3px] font-light text-sm">
         Photo / Videographer
       </h2>
